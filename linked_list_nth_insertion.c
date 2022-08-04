7f5a748cc5028ebd55a5bf65900538787e74b563
Tamim1236 <tamim028@hotmail.com>
2022-08-04 14:29:23 +0600

    Completed Insert_nth() function. There is an error with position variable causing sudden exit. To be fixed.

--- linked_list_nth_insertion.c
+++ linked_list_nth_insertion.c
@@ -15,7 +15,8 @@ void Print();
 int main()
 {
     printf("Enter the number of nodes in your linked list: ");
-    int n,x;
+    int n; //number of elements in the linked list
+    int x; //current number to add to linked list
     scanf("%d", &n);
 
     for(int i = 0; i < n; i++)
@@ -27,12 +28,20 @@ int main()
     printf("Here is your current linked list:\n");
     Print();
 
-    // int num, position;
-    // printf("Now enter a number you want to add to this linked list: ");
-    // scanf("%d", &num);
-    // printf("Enter the position you would like to add it at: ");
-    // scanf("%d", &position);
+    //get new number to add to the linked list
+    int num;
+    int position = -1;
+    printf("Now enter a number you want to add to this linked list: ");
+    scanf("%d", &num);
+    
+    //make sure a valid position is entered
+    while(!(position >=0 && position <= n-1)){
+        printf("Enter the position you would like to add it at: ");
+        scanf("%d", &position);
+    }
 
+    Insert_nth(num, position);
+    Print();
 
     return 0;
 }
@@ -45,6 +54,7 @@ void Print()
         printf(" %d", ptr->value);
         ptr = ptr->next;
     }
+    printf("\n");
 }
 
 //insert a node to the beginning of the list
@@ -57,5 +67,18 @@ void Insert(int value)
 }
 
 void Insert_nth(int value, int position){
-    ;
+    Node* new_node = (Node*) malloc(sizeof(Node));
+    new_node->value = value;
+    new_node->next = NULL;
+    
+    Node* ptr = headptr;
+
+    while(ptr != position - 1)
+    {
+        ptr = ptr->next;
+    }
+
+    new_node->next = ptr->next;
+    ptr->next = new_node;
+
 }
