00a3ae5bfca449763b1b9d155b3d2b7c42d3cc40
Tamim1236 <tamim028@hotmail.com>
2022-08-04 12:07:00 +0600

    Updated struct node and added prototypes for Insert() and Print() functions

--- linked_list_beginning_insertion.c
+++ linked_list_beginning_insertion.c
@@ -2,15 +2,38 @@
 #include <stdlib.h>
 
 //node struct
-struct Node
+typedef struct
 {
     int data;
     struct Node* next;
-};
+}Node;
 
 struct Node* head;
+void Insert(int x);
+void Print();
 
 int main()
 {
-    ;
+    head = NULL; //so far our list is empty
+    printf("How many elements would you like in the list?");
+    int n;
+    int x;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter a number:\n");
+        scanf("%d", &x);
+        Insert(x); //insert this number as a node in linked list
+        Print(); //print all nodes of linked list
+    }
+}
+
+//function to insert node to the beginning
+void Insert(int x)
+{
+    //create a new node
+    Node* temp = (Node*) malloc(sizeof(Node));
+    temp->data = x;
+    temp->next = NULL;
 }
