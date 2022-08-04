e7f409f784126398fb51fbc9931e8eea48400b17
Tamim1236 <tamim028@hotmail.com>
2022-08-04 13:34:34 +0600

    Created file and declared prototypes and struct

---
+++ linked_list_nth_insertion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int value;
+    Node* next;
+}Node;
+
+Node* headptr;
+void Insert(int value);
+void Print();
+
+int main()
+{
+
+    return 0;
+}
