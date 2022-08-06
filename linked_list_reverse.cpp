ef22b8bbc71903dfae3d212435010043c1e73baa
Tamim1236 <tamim028@hotmail.com>
2022-08-06 23:22:24 +0600

    Finished first version of reverse function - for some reason the second value in reversed list doesn't appear

--- linked_list_reverse.cpp
+++ linked_list_reverse.cpp
@@ -29,7 +29,12 @@ int main()
         new_node->next = head;
         head = new_node;
     }
+    
     Print();
+    cout << "Now we will reverse the linked list: " << endl;
+    Reverse();
+    Print();
+
 }
 
 void Print()
@@ -42,4 +47,37 @@ void Print()
         tempptr = tempptr->next;
     }
     cout << "" << endl;
+}
+
+void Reverse()
+{
+    int num_nodes; // number of nodes in the linked list
+    Node* tempptr = head; //used to store final node position
+    
+    while(tempptr->next != NULL) //tempptr->next so that we don't go past last element
+    {
+        num_nodes++;
+        tempptr = tempptr->next;
+    }
+
+    int last_index = num_nodes - 1; //get index of last node in list
+    Node* tempptr2 = tempptr; //used to traverse backwards from end
+
+    while(tempptr2 != head)
+    {
+        last_index -= 1; //to get index of previous
+        int counter = 0; //count up to index
+        Node* prev_location = head; //pointer to record the position value at previous Node
+
+        while(counter != last_index)
+        {
+            prev_location = prev_location->next;
+            counter++;
+        }
+        tempptr2->next = prev_location; //set next of current node to previous node (reverse link)
+        tempptr2 = prev_location; //tempptr now points to previous node
+    }
+
+    tempptr2->next = NULL; //set first node's next to be NULL
+    head = tempptr; //final assignment to set head equal to last element
 }
