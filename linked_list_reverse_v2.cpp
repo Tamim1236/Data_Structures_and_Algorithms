6e8c7747580baa3779436a2f28ea68e38317dde6
Tamim1236 <tamim028@hotmail.com>
2022-08-11 15:55:23 +0400

    Added print and insert and print functions - nodes not being inserted to list for some reason

--- linked_list_reverse_v2.cpp
+++ linked_list_reverse_v2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void Insert();
+void Insert(int value);
 void Print();
 void Reverse();
 
@@ -10,16 +10,48 @@ struct Node{
     Node* next;
 };
 
+Node* head;
+
 int main(){
     
     int num_nodes; 
     cout << "Enter the number of nodes you want in your linked list: ";
     cin >> num_nodes;
 
+    //int num;
     for(int i = 0; i < num_nodes; i++)
     {
-        int num;
+        //int num;
         cout << "Enter a number to add to the linked list: ";
         cin >> num;
+        Insert(num);
+
+        // Node* new_node = new Node();
+        // new_node->data = num;
+        // new_node->next = head->next;
+        // head = new_node;
     }
+
+    Print();
+}
+
+//insert a new node at the beginning of the linked list
+void Insert(int value)
+{
+    Node* new_node = new Node();
+    new_node->data = value;
+    new_node->next = head->next;
+    head = new_node;
+}
+
+void Print(){
+    cout << "Here are the current nodes in the linked list:" << endl;
+    Node* tempptr = head;
+    while(tempptr != NULL)
+    {
+        cout << "printing a node:" << endl;
+        cout << tempptr->data << " ";
+        tempptr = tempptr->next;
+    }
+    cout << "" << endl;
 }
